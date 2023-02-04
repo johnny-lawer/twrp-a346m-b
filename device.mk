5bf07142dc1bfeac5b9a4182cef65159e5c005d4
Alone0316 <rutujamandlik06@gmail.com>
2023-02-04 12:31:03 +0000

    ruby: Import blobs for decryption

--- device.mk
+++ device.mk
@@ -11,5 +11,36 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # fastbootd
 PRODUCT_PACKAGES += \
+<<<<<<< HEAD
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
+=======
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-impl.recovery \
+    android.hardware.boot@1.1-mtkimpl.recovery
+
+PRODUCT_PACKAGES += \
+    bootctrl.mt6877 \
+    bootctrl.mt6877.recovery
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
+# Keymaster
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libkeymaster41 \
+    libpuresoftkeymasterdevice
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+>>>>>>> e4a834d (ruby: Import blobs for decryption)
